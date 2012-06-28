f5ad33f87ad9405fa22048286366533b9de0d375
Peter Gill <peter@majorsilence.com>
2012-06-27 21:36:46 -0230

    Clear $_SESSION['LastUpload'] on logout.

--- logout.php
+++ logout.php
@@ -4,5 +4,6 @@ session_start();
 $_SESSION['LoggedInUser'] = "notset";
 $_SESSION['UserDbId'] = "";
 $_SESSION['UserEmail'] = "";
+$_SESSION['LastUpload'] = "";
 header( 'Location: index.php' ) ;
 ?>

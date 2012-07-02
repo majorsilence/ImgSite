4ffc2979de14e7b5680387688ac62ad6d3a7db52
Peter Gill <peter@majorsilence.com>
2012-07-02 11:46:50 -0230

    Add license information.

--- create_user.php
+++ create_user.php
@@ -1,4 +1,24 @@
 <?php
+
+/*
+* File: create_user.php
+* Author: Peter Gill
+* Copyright: 2012 Peter Gill
+* Link: https://github.com/majorsilence/ImgSite
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details:
+* http://www.gnu.org/licenses/gpl.html
+*
+*/
+
 include("connection_info.php");
 echo "<!DOCTYPE html>\n";
 ?>
--- login_action.php
+++ login_action.php
@@ -1,4 +1,23 @@
 <?php
+/*
+* File: login_action.php
+* Author: Peter Gill
+* Copyright: 2012 Peter Gill
+* Link: https://github.com/majorsilence/ImgSite
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details:
+* http://www.gnu.org/licenses/gpl.html
+*
+*/
+
 include("connection_info.php");
 
 	function login()
